d5d1f7dc4d491d74a765f296deaf38d58f93b1c8
Danny Tang <tang_daniel@outlook.com>
2022-08-17 18:56:11 -0700

    Initial Commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
+import Header from '../components/Header'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <Component {...pageProps} />
+      </div>
+    </>
+  )
 }
 
 export default MyApp
